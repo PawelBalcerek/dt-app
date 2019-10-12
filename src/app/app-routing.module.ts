bbfdb3ba5cae5497ac877c41cf9065695c888d27
HashTagDK <sawy3r11@gmail.com>
2019-10-12 23:10:27 +0200

    -wygenerwoanie projektu -dodanie @angular-materialize -dodanie struktury projektu -dodanie strony home

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }, { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
